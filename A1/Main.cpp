3e4ea678da18bb1bc6116886b7be617ad4c2772d
Jonathan (Xinpeng) Zhao <x67zhao@uwaterloo.ca>
2016-01-14 12:30:45 -0500

    finish assignment polish

--- A1/Main.cpp
+++ A1/Main.cpp
@@ -2,6 +2,6 @@
 
 int main( int argc, char **argv )
 {
-	CS488Window::launch( argc, argv, new A1(), A1::DEFAULT_WIDTH, A1::DEFAULT_HEIGHT, "Assignment 0" );
+	CS488Window::launch( argc, argv, new A1(), A1::DEFAULT_WIDTH, A1::DEFAULT_HEIGHT, "Assignment 1" );
 	return 0;
 }
