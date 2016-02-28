bbdda7158b6191a40c71dd868ae7e213f97a229d
qaovxtazypdl <x67zhao@uwaterloo.ca>
2016-02-28 02:47:38 -0500

    before sleep

--- A4/Assets/nonhier2.lua
+++ A4/Assets/nonhier2.lua
@@ -10,6 +10,7 @@ mat3 = gr.material({1.0, 0.6, 0.1}, {0.5, 0.7, 0.5}, 25)
 mat4 = gr.material({0.7, 0.6, 1.0}, {0.5, 0.4, 0.8}, 25)
 
 scene = gr.node( 'scene' )
+scene:translate(0,0,-800)
 
 s1 = gr.nh_sphere('s1', {0, 0, -400}, 100)
 scene:add_child(s1)
@@ -37,13 +38,13 @@ s5:set_material(mat1)
 
 -- A small stellated dodecahedron.
 
-steldodec = gr.mesh( 'dodec', 'smstdodeca.obj' )
+steldodec = gr.mesh( 'dodec', 'Assets/smstdodeca.obj' )
 steldodec:set_material(mat3)
 scene:add_child(steldodec)
 
 white_light = gr.light({-100.0, 150.0, 400.0}, {0.9, 0.9, 0.9}, {1, 0, 0})
 orange_light = gr.light({400.0, 100.0, 150.0}, {0.7, 0.0, 0.7}, {1, 0, 0})
 
-gr.render(scene, 'nonhier.png', 256, 256,
+gr.render(scene, 'nonhier2.png', 256, 256,
 	  {0, 0, 0}, {0, 0, -1}, {0, 1, 0}, 50,
 	  {0.3, 0.3, 0.3}, {white_light, orange_light})
