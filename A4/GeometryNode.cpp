f043bf3558814c9466e1b178de4ef4ecae3b341e
qaovxtazypdl <x67zhao@uwaterloo.ca>
2016-03-07 23:00:55 -0500

    before heavy debug

--- A4/GeometryNode.cpp
+++ A4/GeometryNode.cpp
@@ -34,14 +34,18 @@ IntersectionInfo GeometryNode::testHit(const dvec4 &ray_origin, const dvec4 &ray
   dmat4 T = trans;
   dmat4 T_inv = invtrans;
   dmat3 T_invtrans = invtrans_transpose;
+cout << "1" << endl;
 
   for (SceneNode * node : children) {
     intersectionInfo.UNION(node->testHit(T_inv * ray_origin, T_inv * ray_dir));
   }
+cout << "2" << endl;
 
   intersectionInfo.UNION(m_primitive->checkRayIntersection(T_inv * ray_origin, T_inv * ray_dir, m_material));
+cout << "3" << endl;
 
   intersectionInfo.TRANSFORM_UP(T, T_invtrans);
+cout << "4" << endl;
 
   return intersectionInfo;
 }
