e0798eab140deacf07d32414ad188a31a154621d
qaovxtazypdl <x67zhao@uwaterloo.ca>
2016-03-12 22:44:37 -0500

    ?

--- A4/Texture.cpp
+++ A4/Texture.cpp
@@ -4,10 +4,27 @@ using namespace glm;
 using namespace std;
 
 Texture::Texture(string filename) {
+  std::vector<unsigned char> image;
   unsigned error = lodepng::decode(image, nx, ny, filename);
   if(error) {
     cout << "decoder error " << error << ": " << lodepng_error_text(error) << endl;
     throw "decoder error";
+  }
+
+  for (int i = 0; i < nx; i++) {
+    grid.push_back(vector<dvec3>(ny));
+  }
+
+  int pos = 0;
+  while (pos < image.size()) {
+    int x = (pos/4) % nx;
+    int y = (pos/4) / nx;
+
+    grid[x][y][0] = image[pos] / 255.0;
+    grid[x][y][1] = image[pos+1] / 255.0;
+    grid[x][y][2] = image[pos+2] / 255.0;
+
+    pos += 4;
   }
 }
 
--- A4/Texture.hpp
+++ A4/Texture.hpp
@@ -8,9 +8,10 @@ class Texture {
 public:
   //file must be png.
   Texture(std::string filename);
+  ~Texture() {}
 
   unsigned nx, ny;
-  std::vector<unsigned char> image;
+  std::vector<std::vector<glm::dvec3>> grid;
 
   glm::dvec3 getColorAt(const std::pair<double, double> &uv);
   glm::dvec4 normalD(const std::pair<double, double> &uv);
