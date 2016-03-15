712042d86ea803a6f820faf8b43f6e935f62c3f1
qaovxtazypdl <x67zhao@uwaterloo.ca>
2016-03-14 23:14:26 -0400

    wtf

--- A4/Texture.cpp
+++ A4/Texture.cpp
@@ -67,6 +67,8 @@ if (!(dot(norm, uvp.Ov) < 1E-6 && dot(norm, uvp.Ou) < 1E-6 && dot(uvp.Ou, uvp.Ov
   cout << endl;
 }*/
 
+  //cout << cross(dvec3(uvp.Ou), dvec3(uvp.Ov)) / dvec3(norm) << endl;
+
   double Bu = (brightness(getColorAt({uvp.uv.first+epsilon, uvp.uv.second}), bump_channel) - brightness(getColorAt({uvp.uv.first-epsilon, uvp.uv.second}), bump_channel)) / 2.0 / epsilon;
   double Bv = (brightness(getColorAt({uvp.uv.first, uvp.uv.second+epsilon}), bump_channel) - brightness(getColorAt({uvp.uv.first, uvp.uv.second-epsilon}), bump_channel)) / 2.0 / epsilon;
   return dvec4(Bu * cross(norm3, dvec3(uvp.Ov)) - Bv * cross(norm3, dvec3(uvp.Ou)), 0.0);
