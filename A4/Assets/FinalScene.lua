b86fc9614091f366be684003d15b5f662f273b5b
qaovxtazypdl <x67zhao@uwaterloo.ca>
2016-03-20 17:22:19 -0400

    knight

--- A4/Assets/FinalScene.lua
+++ A4/Assets/FinalScene.lua
@@ -35,7 +35,13 @@ test_inner_light = gr.light({0,50,50}, {0.4, 0.4, 0.4}, {1,0,0})
 scene = gr.node('root')
 
 walls = gr.node('walls')
+pillars = gr.node('pillars')
+altar = gr.node('altar')
+statues = gr.node('statues')
 --scene:add_child(walls)
+--scene:add_child(pillars)
+--scene:add_child(altar)
+scene:add_child(statues)
 
 
 ------------------------------- WINDOW
@@ -216,9 +222,6 @@ lightholder_left:add_child(light_left)
 --=========================pillar========================
 
 --===========================pillars
-pillars = gr.node('pillars')
---scene:add_child(pillars)
-
 left_pillar_1 = gr.node('left_pillar_1')
 left_pillar_1:rotate('y', 180)
 left_pillar_1:translate(-66,15.5,-235)
@@ -254,9 +257,6 @@ pillars:add_child(right_pillar_3)
 --==================================================
 
 --=====altar==================================
-altar = gr.node('altar')
-scene:add_child(altar)
-
 altar_base_1 = gr.cube('altar_base_1')
 altar_base_1:translate(-0.5, -0.5, -0.5)
 altar_base_1:scale(200,11,100)
@@ -279,14 +279,13 @@ altar_base_3:set_material(chrome)
 altar:add_child(altar_base_3)
 
 artifact = gr.node('artifact')
-artifact:translate(0,-80,-150)
+artifact:translate(5,-47,-350)
 altar:add_child(artifact)
 
 artifact_base = gr.cylinder('artifact_base')
 artifact_base:scale(50,6,50)
 artifact_base:set_material(pearl)
 artifact:add_child(artifact_base)
-
 
 artifact_block = gr.cube('artifact_block')
 artifact_block:translate(-0.5,-0.5,-0.5)
@@ -321,19 +320,264 @@ sphericalcarver:translate(0,30,0)
 sphericalcarver:set_material(ruby)
 
 carvedpyramid = gr.csg('carvedpyramid')
-carvedpyramid:rotate('x', 180)
-carvedpyramid:translate(0,100,0)
+carvedpyramid:rotate('x', 90)
+carvedpyramid:rotate('y', -90)
+carvedpyramid:translate(-32,90,0)
+carvedpyramid:scale(0.55,0.55,0.55)
 carvedpyramid:set_csg_children(pyramid, sphericalcarver, 'DIFFERENCE')
 artifact:add_child(carvedpyramid)
 
+carvedpyramid_2 = gr.csg('carvedpyramid_2')
+carvedpyramid_2:rotate('x', -90)
+carvedpyramid_2:rotate('y', -90)
+carvedpyramid_2:translate(32,90,0)
+carvedpyramid_2:scale(0.55,0.55,0.55)
+carvedpyramid_2:set_csg_children(pyramid, sphericalcarver, 'DIFFERENCE')
+artifact:add_child(carvedpyramid_2)
 
 artifact_sphere = gr.sphere('artifact_sphere')
 artifact_sphere:scale(24,24,24)
-artifact_sphere:translate(0,100,0)
+artifact_sphere:translate(0,50,0)
 artifact_sphere:set_material(chrome)
 artifact:add_child(artifact_sphere)
 
+artifact_cylinder = gr.cylinder('artifact_cylinder')
+artifact_cylinder:scale(35,0.5,35)
+artifact_cylinder:rotate('z', 61)
+artifact_cylinder:rotate('y', 12)
+artifact_cylinder:translate(0,50,0)
+artifact_cylinder:set_material(chrome)
+
+carvedring = gr.csg('carvedring')
+carvedring:scale(1.3,1.3,1.3)
+carvedring:translate(0,-15,0)
+carvedring:set_csg_children(artifact_cylinder, artifact_sphere, 'DIFFERENCE')
+artifact:add_child(carvedring)
 --===========================================
+
+
+--===========================================STATUES
+i_statue = gr.node('i_statue')
+i_statue:translate(0,0,-100)
+statues:add_child(i_statue)
+
+--helmet
+s_helmet = gr.cylinder('s_helmet')
+s_helmet:scale(8.5,10,8.5)
+s_helmet:translate(0,10,0)
+s_helmet:set_material(chrome)
+
+s_helmet_diff = gr.node('s_helmet_diff')
+
+s_helmet_inner = gr.cylinder('s_helmet_inner')
+s_helmet_inner:scale(7.8,10,7.8)
+s_helmet_inner:translate(0,9,0)
+s_helmet_inner:set_material(ruby)
+s_helmet_diff:add_child(s_helmet_inner)
+
+s_helmet_eyehole = gr.cylinder('s_helmet_eyehole')
+s_helmet_eyehole:scale(2.7,5,2.7)
+s_helmet_eyehole:rotate('z', 90)
+s_helmet_eyehole:translate(0,10,6.6)
+s_helmet_eyehole:set_material(ruby)
+s_helmet_diff:add_child(s_helmet_eyehole)
+
+s_helmet_mouthhole = gr.cylinder('s_helmet_mouthhole')
+s_helmet_mouthhole:scale(2.7,5,2.7)
+s_helmet_mouthhole:translate(0,5,6)
+s_helmet_mouthhole:set_material(emerald)
+s_helmet_diff:add_child(s_helmet_mouthhole)
+
+s_helmet_csg = gr.csg('s_helmet_csg')
+s_helmet_csg:translate(0,23,0)
+s_helmet_csg:set_csg_children(s_helmet, s_helmet_diff, 'DIFFERENCE')
+i_statue:add_child(s_helmet_csg)
+
+--body
+s_body_frame = gr.hyperboloid('s_body_frame', 8)
+s_body_frame:scale(6,18,3.8)
+s_body_frame:set_material(chrome)
+i_statue:add_child(s_body_frame)
+
+s_body_trim_top = gr.torus('s_body_trim_top', 0.07)
+s_body_trim_top:scale(17.3,11.7,17.3)
+s_body_trim_top:rotate('x', 90)
+s_body_trim_top:translate(0,18,0)
+s_body_trim_top:set_material(emerald)
+i_statue:add_child(s_body_trim_top)
+
+s_body_trim_bottom = gr.torus('s_body_trim_bottom', 0.07)
+s_body_trim_bottom:scale(17.3,11.7,17.3)
+s_body_trim_bottom:rotate('x', 90)
+s_body_trim_bottom:translate(0,-18,0)
+s_body_trim_bottom:set_material(emerald)
+i_statue:add_child(s_body_trim_bottom)
+
+--legs
+leg = gr.node('leg')
+
+thigh_socket = gr.sphere('thigh_socket')
+thigh_socket:scale(6.4,6.4,6.4)
+thigh_socket:translate(0,-20,0)
+thigh_socket:set_material(ruby)
+leg:add_child(thigh_socket)
+
+thigh = gr.cone('thigh')
+thigh:rotate('x', 180)
+thigh:scale(6.8,100,6.8)
+thigh:translate(0,-123,0)
+thigh:set_material(emerald)
+
+thigh_bounds = gr.cylinder('thigh_bounds')
+thigh_bounds:scale(6.3,12,6.3)
+thigh_bounds:translate(0,-35,0)
+thigh_bounds:set_material(chrome)
+
+lthigh = gr.csg('lthigh')
+lthigh:set_csg_children(thigh, thigh_bounds, 'INTERSECT')
+leg:add_child(lthigh)
+
+kneecap = gr.sphere('kneecap')
+kneecap:scale(5.5,5.5,5.5)
+kneecap:translate(0,-47,0)
+kneecap:set_material(chrome)
+leg:add_child(kneecap)
+
+shin = gr.cylinder('shin')
+shin:scale(4.8,12,4.8)
+shin:translate(0,-58,0)
+shin:set_material(chrome)
+leg:add_child(shin)
+
+heel = gr.sphere('heel')
+heel:scale(4.7,4.7,4.7)
+heel:translate(0,-70,0)
+heel:set_material(ruby)
+leg:add_child(heel)
+
+shoe = gr.cube('shoe')
+shoe:translate(-0.5,-0.5,-0.5)
+shoe:scale(9,4,18)
+shoe:translate(0,-75,6)
+shoe:set_material(emerald)
+
+shoe_differ = gr.cube('shoe_differ')
+shoe_differ:translate(-0.5,-0.5,-0.5)
+shoe_differ:scale(10,4,18)
+shoe_differ:rotate('x', 25)
+shoe_differ:translate(0,-73,13)
+shoe_differ:set_material(ruby)
+
+shoecsg = gr.csg('shoecsg')
+shoecsg:set_csg_children(shoe, shoe_differ, 'DIFFERENCE')
+leg:add_child(shoecsg)
+
+left_leg = gr.node('left_leg')
+left_leg:rotate('y', -15)
+left_leg:translate(-9,0,0)
+left_leg:add_child(leg)
+i_statue:add_child(left_leg)
+
+right_leg = gr.node('right_leg')
+right_leg:rotate('y', 15)
+right_leg:translate(9,0,0)
+right_leg:add_child(leg)
+i_statue:add_child(right_leg)
+
+
+--s_body_frame = gr.hyperboloid('s_body_frame', 8)
+--s_body_frame:scale(6,18,6)
+--s_body_frame:set_material(chrome)
+--i_statue:add_child(s_body_frame)
+
+--------arms
+left_arm = gr.node('left_arm')
+i_statue:add_child(left_arm)
+
+shoulder = gr.sphere('shoulder')
+shoulder:scale(6,6,6)
+shoulder:translate(-18,18,0)
+shoulder:set_material(jade)
+left_arm:add_child(shoulder)
+
+arm_top = gr.cone('arm_top')
+arm_top:scale(6.5,80,6.5)
+arm_top:rotate('x', 180)
+arm_top:translate(-18,-59,0)
+arm_top:set_material(emerald)
+
+arm_top_bounds = gr.cube('arm_top_bounds')
+arm_top_bounds:translate(-0.5,-0.5,-0.5)
+arm_top_bounds:scale(10,25,10)
+arm_top_bounds:translate(-18,8,0)
+arm_top_bounds:set_material(ruby)
+
+armtop_csg = gr.csg('armtop_csg')
+armtop_csg:translate(0,-9,0)
+armtop_csg:rotate('z', -50)
+armtop_csg:rotate('y',20)
+armtop_csg:translate(-16,-4,0)
+armtop_csg:set_csg_children(arm_top, arm_top_bounds, 'INTERSECT')
+left_arm:add_child(armtop_csg)
+
+elbow = gr.sphere('elbow')
+elbow:scale(5,5,5)
+elbow:rotate('y',-42)
+elbow:translate(-18,-7.6,0)
+elbow:set_material(ruby)
+armtop_csg:add_child(elbow)
+
+arm = gr.cylinder('arm')
+arm:scale(1/5,1/5,1/5)
+arm:scale(3.5,20,3.5)
+arm:translate(0,4,0)
+arm:rotate('z', -55)
+arm:set_material(emerald)
+elbow:add_child(arm)
+------------
+
+right_arm = gr.node('right_arm')
+right_arm:scale(-1,1,1)
+right_arm:add_child(left_arm)
+i_statue:add_child(right_arm)
+
+hands = gr.sphere('hands')
+hands:scale(5,5,5)
+hands:translate(0,-5,17)
+hands:set_material(chrome)
+i_statue:add_child(hands)
+
+--sword
+handle = gr.cylinder('handle')
+handle:scale(1/5,1/5,1/5)
+handle:scale(1,7,1)
+handle:translate(0,0,0)
+handle:set_material(ruby)
+hands:add_child(handle)
+
+hilt = gr.cylinder('hilt')
+hilt:scale(1/5,1/5,1/5)
+hilt:scale(1.6,8.8,1.6)
+hilt:rotate('z', 90)
+hilt:translate(0,-1.2,0)
+hilt:set_material(ruby)
+hands:add_child(hilt)
+
+blade = gr.cube('blade')
+blade:translate(-0.5,-0.5,-0.5)
+blade:scale(1/5,1/5,1/5)
+blade:scale(12,70,4)
+blade:translate(0,-8,0)
+blade:set_material(turquoise)
+hands:add_child(blade)
+-------
+
+statue_base = gr.cylinder('statue_base')
+statue_base:scale(36,1.5,36)
+statue_base:translate(0,-79,0)
+statue_base:set_material(chrome)
+i_statue:add_child(statue_base)
+--====================================================
 
 gr.render(scene, 'FinalScene.png', 700, 500,
     {0, 0, 100}, {0, 0, -100}, {0, 1, 0}, 50,
